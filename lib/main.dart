03cbb287be77c8823ad826d99091108c8cc1d69c
Keshav Kumar <keshavkumar8738@gmail.com>
2023-02-09 23:35:58 +0530

    1hq

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,12 @@
 import 'package:crm_msocial/HomePage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
   // await FlutterDownloader.initialize(
   //     debug: true // optional: set false to disable printing logs to console
   // );
-  await Permission.storage.request();
   runApp(const App());
 }
 
